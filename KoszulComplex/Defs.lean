0d1dd857281ed87446ff146e8db1688cbf35de84
syur2 <sif4delta0@mail.ustc.edu.cn>
2025-04-06 17:01:48 +0800

    Update Defs.lean

--- KoszulComplex/Defs.lean
+++ KoszulComplex/Defs.lean
@@ -15,6 +15,22 @@ noncomputable def ModuleCat.exteriorPower.contraction_aux :
 noncomputable def ModuleCat.exteriorPower.contraction : L.exteriorPower (n + 1) ⟶ L.exteriorPower n :=
   desc (contraction_aux L f n)
 
+#check AlgebraicTopology.AlternatingFaceMapComplex.d_squared
+#check map_sum
+
 noncomputable def koszulComplex : ChainComplex (ModuleCat R) ℕ := by
   refine ChainComplex.of L.exteriorPower (ModuleCat.exteriorPower.contraction L f) (fun n ↦ ?_)
+  ext x
+  simp only [ModuleCat.AlternatingMap.postcomp_apply, ModuleCat.hom_comp, LinearMap.coe_comp,
+    Function.comp_apply, ModuleCat.hom_zero, LinearMap.zero_apply]
+  unfold ModuleCat.exteriorPower.contraction-- ModuleCat.exteriorPower.contraction_aux
+  simp only [ModuleCat.exteriorPower.desc_mk]
+  rw [iaob]
+  -- need map_finsum
+  have : (ModuleCat.Hom.hom (ModuleCat.exteriorPower.desc (ModuleCat.exteriorPower.contraction_aux L f n)))
+    (∑ᶠ (i : Fin (n + 1 + 1)), ((-1 : R) ^ i.1 * f (x i)) • (exteriorPower.ιMulti R (n + 1)) (x ∘ i.succAbove)) =
+    ∑ᶠ (i : Fin (n + 1 + 1)), (ModuleCat.Hom.hom (ModuleCat.exteriorPower.desc (ModuleCat.exteriorPower.contraction_aux L f n))) (((-1 : R) ^ i.1 * f (x i)) • (exteriorPower.ιMulti R (n + 1)) (x ∘ i.succAbove))
+    --∑ᶠ (i : Fin (n + 1 + 1)), ((-1 : R) ^ i.1 * f (x i)) • ((ModuleCat.Hom.hom (ModuleCat.exteriorPower.desc (ModuleCat.exteriorPower.contraction_aux L f n))) ((exteriorPower.ιMulti R (n + 1)) (x ∘ i.succAbove)))
+     := by
+    sorry
   sorry
