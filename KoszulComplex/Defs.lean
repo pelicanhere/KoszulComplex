43d5476151fe62ba92f4d121c5131bdcefe99eec
Yongle Hu <140475041+mbkybky@users.noreply.github.com>
2025-04-06 17:39:25 +0800

    Update Defs.lean

--- KoszulComplex/Defs.lean
+++ KoszulComplex/Defs.lean
@@ -1,47 +1,69 @@
 import Mathlib
 
-variable {R : Type*} [CommRing R] (L : ModuleCat R) (f : L →ₗ[R] R) (M : ModuleCat R) (n : ℕ)
+variable {R : Type*} [CommRing R] (L : Type*) [AddCommGroup L] [Module R L] (f : L →ₗ[R] R) (n : ℕ)
 
-open ExteriorAlgebra
+open ExteriorAlgebra ModuleCat
 
-noncomputable def ModuleCat.exteriorPower.contraction_aux :
-    L.AlternatingMap (L.exteriorPower n) (n + 1) where
+noncomputable def exteriorPower.contraction_aux : AlternatingMap R L (⋀[R]^n L) (Fin (n + 1)) where
   toFun x := ∑ᶠ i : Fin (n + 1),
     ((- 1 : R) ^ i.1 * (f (x i))) • exteriorPower.ιMulti R n (x.comp (Fin.succAbove i))
   map_update_add' := sorry
   map_update_smul' := sorry
   map_eq_zero_of_eq' := sorry
 
-noncomputable def ModuleCat.exteriorPower.contraction : L.exteriorPower (n + 1) ⟶ L.exteriorPower n :=
-  desc (contraction_aux L f n)
+noncomputable def ModuleCat.exteriorPower.contraction :
+    (of R L).exteriorPower (n + 1) ⟶ (of R L).exteriorPower n :=
+  desc (exteriorPower.contraction_aux L f n)
 
 #check AlgebraicTopology.AlternatingFaceMapComplex.d_squared
 #check map_sum
 
-noncomputable def koszulComplex : ChainComplex (ModuleCat R) ℕ := by
-  refine ChainComplex.of L.exteriorPower (ModuleCat.exteriorPower.contraction L f) (fun n ↦ ?_)
+noncomputable def koszulComplex : HomologicalComplex (ModuleCat R) (ComplexShape.down ℕ) := by
+  refine ChainComplex.of (of R L).exteriorPower (ModuleCat.exteriorPower.contraction L f) (fun n ↦ ?_)
   ext x
   simp only [ModuleCat.AlternatingMap.postcomp_apply, ModuleCat.hom_comp, LinearMap.coe_comp,
     Function.comp_apply, ModuleCat.hom_zero, LinearMap.zero_apply]
   unfold ModuleCat.exteriorPower.contraction-- ModuleCat.exteriorPower.contraction_aux
   simp only [ModuleCat.exteriorPower.desc_mk]
-  rw [iaob]
+  /- rw [iaob]
   -- need map_finsum
   have : (ModuleCat.Hom.hom (ModuleCat.exteriorPower.desc (ModuleCat.exteriorPower.contraction_aux L f n)))
     (∑ᶠ (i : Fin (n + 1 + 1)), ((-1 : R) ^ i.1 * f (x i)) • (exteriorPower.ιMulti R (n + 1)) (x ∘ i.succAbove)) =
     ∑ᶠ (i : Fin (n + 1 + 1)), (ModuleCat.Hom.hom (ModuleCat.exteriorPower.desc (ModuleCat.exteriorPower.contraction_aux L f n))) (((-1 : R) ^ i.1 * f (x i)) • (exteriorPower.ιMulti R (n + 1)) (x ∘ i.succAbove))
     --∑ᶠ (i : Fin (n + 1 + 1)), ((-1 : R) ^ i.1 * f (x i)) • ((ModuleCat.Hom.hom (ModuleCat.exteriorPower.desc (ModuleCat.exteriorPower.contraction_aux L f n))) ((exteriorPower.ιMulti R (n + 1)) (x ∘ i.succAbove)))
      := by
-    sorry
+    sorry -/
   sorry
 
 /-- The Koszul homology $H_n(f)$. -/
 noncomputable def koszulHomology : ModuleCat R := (koszulComplex L f).homology n
 
+namespace RingTheory.Sequence
+
+variable (rs : List R)
+
+/-- Let $\mathbf{x} = (x_1, \dots, x_n)$ be a sequence in $R$, consider
+  $f_{\mathbf{x}} : R^n, e_i \mapsto x_i$. The Koszul complex $K_{\bullet}(\mathbf{x})$
+  is defined as $K_{\bullet}(\mathbf{x}) := K_{\bullet}(f_{\mathbf{x}})$. -/
+noncomputable def koszulComplex : HomologicalComplex (ModuleCat R) (ComplexShape.down ℕ) :=
+  _root_.koszulComplex (Fin rs.length →₀ R) <|
+    Finsupp.linearCombination R (fun (i : Fin rs.length) ↦ rs.get i)
+
+end RingTheory.Sequence
+
+section twisted
+
+universe u
+
+variable {R : Type u} [CommRing R] (L : Type u) [AddCommGroup L] [Module R L] (f : L →ₗ[R] R)
+  (M : ModuleCat.{u} R) (n : ℕ)
+
 /-- The Koszul complex with coefficients in $M$ is defined as $K_{\bullet}(f) := K_{\bullet}(f)⊗M$. -/
-noncomputable def twistedKoszulComplex : ChainComplex (ModuleCat R) ℕ :=
+noncomputable def twistedKoszulComplex : HomologicalComplex (ModuleCat R) (ComplexShape.down ℕ) :=
   ((CategoryTheory.MonoidalCategory.tensorRight M).mapHomologicalComplex (ComplexShape.down ℕ)).obj
     (koszulComplex L f)
 
-/-- The Koszul homology with coefficients in $M$ is defined as $H_n(f, M)$. -/
+/-- $H_n(f, M)$ is the Koszul homology with coefficients in $M$ is defined as . -/
 noncomputable def twistedKoszulHomology : ModuleCat R := (twistedKoszulComplex L f M).homology n
+
+end twisted
