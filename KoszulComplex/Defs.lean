8ed7aca2236092b4e2b20ec06cbb6a7441fd2fd5
yuanyi-350 <kysyy1@126.com>
2025-04-23 18:44:52 +0800

    finish all

--- KoszulComplex/Defs.lean
+++ KoszulComplex/Defs.lean
@@ -9,52 +9,16 @@ lemma Z_simp (R : Type u) [Ring R] {L : Type v} [AddCommGroup L] [Module R L]
   · simp [ch]
   · simp [ch]
 
+instance lt_ne {n : ℕ}{i j : Fin (n + 1)} (hij : i < j) : NeZero n := by
+  by_contra ne
+  have : n = 0 := not_neZero.mp ne
+  have : j.1 < n + 1 := j.isLt
+  have : i.1 ≥ 0 := Nat.zero_le ↑i
+  omega
+
 variable {R : Type u} [CommRing R] (L : Type v) [AddCommGroup L] [Module R L] (f : L →ₗ[R] R) (n : ℕ)
 
 open ExteriorAlgebra ModuleCat CategoryTheory Fin Function
-
-noncomputable def exteriorPower.contraction_aux : AlternatingMap R L (⋀[R]^n L) (Fin (n + 1)) where
-  toFun x := ∑ᶠ i : Fin (n + 1),
-    ((- 1 : R) ^ i.1 * (f (x i))) • exteriorPower.ιMulti R n (x.comp i.succAbove)
-  map_update_add' m i x y := by
-    rw [finsum_eq_sum_of_fintype, finsum_eq_sum_of_fintype, finsum_eq_sum_of_fintype,
-      ← Finset.sum_add_distrib]
-    congr
-    funext w
-    rw [mul_smul, mul_smul, mul_smul, ← smul_add]
-    congr
-    by_cases hw : w = i
-    · rw [hw, update_self, update_self, update_self, map_add, add_smul]
-      congr <;> (funext k ; rw [comp_apply, comp_apply, update_of_ne (i.succAbove_ne k),
-        update_of_ne (i.succAbove_ne k)])
-    · rw [update_of_ne hw, update_of_ne hw, update_of_ne hw, ← smul_add]
-      congr
-      obtain ⟨j, hj⟩ : ∃ j : Fin n, w.succAbove j = i :=
-        exists_succAbove_eq (show w ≠ i by exact hw).symm
-      have := hj ▸ (update_comp_eq_of_injective m (succAbove_right_injective (p := w)) j)
-      rw [this _, this _, this _, AlternatingMap.map_update_add]
-  map_update_smul' m i r x := by
-    rw [finsum_eq_sum_of_fintype, finsum_eq_sum_of_fintype, Finset.smul_sum]
-    congr
-    funext j
-    by_cases jeqi : j = i
-    · rw [jeqi, update_self, update_self, map_smul, smul_eq_mul, ← smul_assoc, smul_eq_mul,
-        ← mul_assoc, ← mul_assoc, mul_comm _ r]
-      congr
-      funext k
-      simp only [comp_apply, ne_eq, succAbove_ne i k, not_false_eq_true, update_of_ne]
-    · simp only [ne_eq, jeqi, not_false_eq_true, update_of_ne, SetLike.val_smul, exteriorPower.ιMulti_apply_coe]
-      rw [smul_comm]
-      congr
-      obtain ⟨l, hl⟩ : ∃ l : Fin n, j.succAbove l = i :=
-        exists_succAbove_eq (show j ≠ i by exact jeqi).symm
-      have := hl ▸ (update_comp_eq_of_injective m (succAbove_right_injective (p := j)) l)
-      rw [this _, this _, AlternatingMap.map_update_smul]
-  map_eq_zero_of_eq' x i j eq neq := by
-    wlog le : i ≤ j
-    · simp [neq] at le
-      exact this L f n x j i eq.symm neq.symm (le_of_lt le)
-    sorry
 
 omit [AddCommGroup L] in
 theorem succAbove_comp_cycleIcc [NeZero n] (x : Fin (n + 1) → L) (i j : Fin (n + 1)) (eq : x i = x j)
@@ -71,7 +35,6 @@ theorem succAbove_comp_cycleIcc [NeZero n] (x : Fin (n + 1) → L) (i j : Fin (n
       omega
     simp [cycleIcc_of_le hij ch1, this]
   simp at ch ch1
-  have hik: i.castLT ilt ≤ k := le_def.mpr (by simpa)
   by_cases ch2 : k < (j - 1).castLT jlt
   · have : (k + 1).castSucc < j := by
       rw [lt_def, coe_castSucc, val_add_one_of_lt' ch2]
@@ -87,17 +50,17 @@ theorem succAbove_comp_cycleIcc [NeZero n] (x : Fin (n + 1) → L) (i j : Fin (n
     (by simp [← le_antisymm ch2 ch1, val_sub_one_of_ne_zero (ne_zero_of_lt lt)]; omega)
 
 lemma step1 [NeZero n] (x : Fin (n + 1) → L) (i j : Fin (n + 1)) (eq : x i = x j) (lt : i < j) :
-    ((-1) ^ i.1 * f (x i)) • (ιMulti R n) (x ∘ i.succAbove)
-    + ((-1) ^ j.1 * f (x j)) • (ιMulti R n) (x ∘ j.succAbove) = 0 := by
+    ((-1) ^ i.1 * f (x i)) • (exteriorPower.ιMulti R n) (x ∘ i.succAbove)
+    + ((-1) ^ j.1 * f (x j)) • (exteriorPower.ιMulti R n) (x ∘ j.succAbove) = 0 := by
   have ilt : i.1 < n := by omega
   have jlt : (j - 1).1 < n := by rw [val_sub_one_of_ne_zero (ne_zero_of_lt lt)]; omega
   have hij : castLT i ilt ≤ castLT (j - 1) jlt := by
     simp [le_def, val_sub_one_of_ne_zero (ne_zero_of_lt lt)]; omega
   rw [succAbove_comp_cycleIcc L n x i j eq ilt jlt lt hij]
-  show ((-1) ^ i.1 * f (x i)) • ((ιMulti R n) ((x ∘ j.succAbove) ∘ ⇑(cycleIcc hij)))
-      + ((-1) ^ j.1 * f (x j)) • (ιMulti R n) (x ∘ j.succAbove) = 0
-  simp only [eq, AlternatingMap.map_perm (ιMulti R n) (x ∘ j.succAbove) (cycleIcc hij),
-    sign_cycleIcc, coe_castLT, Z_simp R ((j - 1).1 - i.1) ((ιMulti R n) (x ∘ j.succAbove))]
+  show ((-1) ^ i.1 * f (x i)) • ((exteriorPower.ιMulti R n) ((x ∘ j.succAbove) ∘ ⇑(cycleIcc hij)))
+      + ((-1) ^ j.1 * f (x j)) • (exteriorPower.ιMulti R n) (x ∘ j.succAbove) = 0
+  simp only [eq, AlternatingMap.map_perm (exteriorPower.ιMulti R n) (x ∘ j.succAbove) (cycleIcc hij),
+    sign_cycleIcc, coe_castLT, Z_simp R ((j - 1).1 - i.1) ((exteriorPower.ιMulti R n) (x ∘ j.succAbove))]
   have : (-1 : R) ^ i.1 * (-1 : R) ^ ((j - 1).1 - i.1) + (-1) ^ j.1 = 0 := by
     have : i.1 + (j.1 - 1 - i.1) = j.1 - 1 := by omega
     have part: (-1 : R) ^ (j.1 - 1) * (-1) ^ 1 = (-1 : R) ^ (j.1) := by
@@ -142,9 +105,9 @@ lemma left_inv (k : Fin (n + 1))(i : {x : Fin (n + 1) | x ≠ k}): k.succAbove (
     have : k < i.1 := lt_of_le_of_ne ch (Subtype.coe_prop i).symm
     omega
 
-lemma step2 [NeZero n] (x : Fin (n + 1) → L) (i j k : Fin (n + 1)) (eq : x i = x j) (neq : i ≠ j)
-    (hk : k ≠ i ∧ k ≠ j): ((-1) ^ k.1 * f (x k)) • (ιMulti R n) (x ∘ k.succAbove) = 0 := by
-  have : (ιMulti R n) (x ∘ k.succAbove) = 0 := by
+lemma step2 (x : Fin (n + 1) → L) (i j k : Fin (n + 1)) (eq : x i = x j) (neq : i ≠ j)
+    (hk : k ≠ i ∧ k ≠ j): ((-1) ^ k.1 * f (x k)) • (exteriorPower.ιMulti R n) (x ∘ k.succAbove) = 0 := by
+  have : (exteriorPower.ιMulti R n) (x ∘ k.succAbove) = 0 := by
     have i_in : i ∈ {x : Fin (n + 1) | x ≠ k} := Set.mem_setOf.mpr (fun a ↦ hk.1 (id (a.symm)))
     have j_in : j ∈ {x : Fin (n + 1) | x ≠ k} := Set.mem_setOf.mpr (fun a ↦ hk.2 (id (a.symm)))
     have eq : (x ∘ k.succAbove) (aux n k ⟨i, i_in⟩) = (x ∘ k.succAbove) (aux n k ⟨j, j_in⟩) := by
@@ -153,10 +116,80 @@ lemma step2 [NeZero n] (x : Fin (n + 1) → L) (i j k : Fin (n + 1)) (eq : x i =
     have neq : (aux n k ⟨i, i_in⟩) ≠ (aux n k ⟨j, j_in⟩) := fun eq ↦ by
       have := aux_inj n k eq
       simp[neq] at this
-    exact AlternatingMap.map_eq_zero_of_eq (ιMulti R n) (x ∘ k.succAbove) eq neq
+    exact AlternatingMap.map_eq_zero_of_eq (exteriorPower.ιMulti R n) (x ∘ k.succAbove) eq neq
   rw [mul_smul]
-  show (-1) ^ k.1 • (f (x k) • (ιMulti R n) (x ∘ k.succAbove)) = 0
+  show (-1) ^ k.1 • (f (x k) • (exteriorPower.ιMulti R n) (x ∘ k.succAbove)) = 0
   simp [this]
+
+lemma sum_of_two (s : Fin n → L)(i j : Fin n) (neq : i ≠ j)(eq0 : ∀ k, k ≠ i ∧ k ≠ j → s k = 0)
+    (sum0 : s i + s j = 0) : finsum s = 0 := by
+  by_cases ch : s i = 0
+  · have : ∀ k, s k = 0 := fun k ↦ by
+      by_cases ch2 : k ≠ i ∧ k ≠ j
+      · exact eq0 k ch2
+      · rcases Decidable.or_iff_not_not_and_not.mpr ch2 with ch3 | ch3
+        · rw [ch3, ch]
+        · simpa [ch3, ch] using sum0
+    exact finsum_eq_zero_of_forall_eq_zero this
+  · have : s.support = {i, j} := by
+      refine support_eq_iff.mpr ?_
+      constructor
+      · intro x hx
+        rcases hx with ch3 | ch3
+        · simp [ch3, ch]
+        · rw [ch3, ← neg_eq_of_add_eq_zero_right sum0]
+          simp [ch]
+      · intro x hx
+        exact eq0 x (not_or.mp hx)
+    rw [← finsum_mem_support s, this, finsum_mem_pair neq, sum0]
+
+noncomputable def exteriorPower.contraction_aux : AlternatingMap R L (⋀[R]^n L) (Fin (n + 1)) where
+  toFun x := ∑ᶠ i : Fin (n + 1),
+    ((- 1 : R) ^ i.1 * (f (x i))) • exteriorPower.ιMulti R n (x.comp i.succAbove)
+  map_update_add' m i x y := by
+    rw [finsum_eq_sum_of_fintype, finsum_eq_sum_of_fintype, finsum_eq_sum_of_fintype,
+      ← Finset.sum_add_distrib]
+    congr
+    funext w
+    rw [mul_smul, mul_smul, mul_smul, ← smul_add]
+    congr
+    by_cases hw : w = i
+    · rw [hw, update_self, update_self, update_self, map_add, add_smul]
+      congr <;> (funext k ; rw [comp_apply, comp_apply, update_of_ne (i.succAbove_ne k),
+        update_of_ne (i.succAbove_ne k)])
+    · rw [update_of_ne hw, update_of_ne hw, update_of_ne hw, ← smul_add]
+      congr
+      obtain ⟨j, hj⟩ : ∃ j : Fin n, w.succAbove j = i :=
+        exists_succAbove_eq (show w ≠ i by exact hw).symm
+      have := hj ▸ (update_comp_eq_of_injective m (succAbove_right_injective (p := w)) j)
+      rw [this _, this _, this _, AlternatingMap.map_update_add]
+  map_update_smul' m i r x := by
+    rw [finsum_eq_sum_of_fintype, finsum_eq_sum_of_fintype, Finset.smul_sum]
+    congr
+    funext j
+    by_cases jeqi : j = i
+    · rw [jeqi, update_self, update_self, map_smul, smul_eq_mul, ← smul_assoc, smul_eq_mul,
+        ← mul_assoc, ← mul_assoc, mul_comm _ r]
+      congr
+      funext k
+      simp only [comp_apply, ne_eq, succAbove_ne i k, not_false_eq_true, update_of_ne]
+    · simp only [ne_eq, jeqi, not_false_eq_true, update_of_ne, SetLike.val_smul, exteriorPower.ιMulti_apply_coe]
+      rw [smul_comm]
+      congr
+      obtain ⟨l, hl⟩ : ∃ l : Fin n, j.succAbove l = i :=
+        exists_succAbove_eq (show j ≠ i by exact jeqi).symm
+      have := hl ▸ (update_comp_eq_of_injective m (succAbove_right_injective (p := j)) l)
+      rw [this _, this _, AlternatingMap.map_update_smul]
+  map_eq_zero_of_eq' x i j eq neq := by
+    wlog le : i ≤ j
+    · simp [neq] at le
+      exact this L f n x j i eq.symm neq.symm (le_of_lt le)
+    set s := fun i ↦ ((-1) ^ i.1 * f (x i)) • (ιMulti R n) (x ∘ i.succAbove) with hs
+    have hij : i < j := lt_of_le_of_ne le neq
+    have : NeZero n := lt_ne hij
+    have sum0 : s i + s j = 0 := step1 L f n x i j eq hij
+    have eq0 : ∀ k, k ≠ i ∧ k ≠ j → s k = 0 := fun k ↦ step2 L f n x i j k eq neq
+    exact sum_of_two (↥(⋀[R]^n L)) (n + 1) s i j neq eq0 sum0
 
 noncomputable def ModuleCat.exteriorPower.contraction :
     (of R L).exteriorPower (n + 1) ⟶ (of R L).exteriorPower n :=
@@ -183,51 +216,51 @@ noncomputable def koszulComplex :
     sorry -/
   sorry
 
-/-- The Koszul homology $H_n(f)$. -/
-noncomputable def koszulHomology : ModuleCat R := (koszulComplex L f).homology n
+-- /-- The Koszul homology $H_n(f)$. -/
+-- noncomputable def koszulHomology : ModuleCat R := (koszulComplex L f).homology n
 
-namespace RingTheory.Sequence
+-- namespace RingTheory.Sequence
 
-variable (rs : List R)
+-- variable (rs : List R)
 
-/-- Let $\mathbf{x} = (x_1, \dots, x_n)$ be a sequence in $R$, consider
-  $f_{\mathbf{x}} : R^n \to R, e_i \mapsto x_i$. The Koszul complex $K_{\bullet}(\mathbf{x})$
-  is defined as $K_{\bullet}(\mathbf{x}) := K_{\bullet}(f_{\mathbf{x}})$. -/
-noncomputable def koszulComplex : HomologicalComplex (ModuleCat R) (ComplexShape.down ℕ) :=
-  _root_.koszulComplex (Fin rs.length →₀ R) <|
-    Finsupp.linearCombination R (fun (i : Fin rs.length) ↦ rs.get i)
+-- /-- Let $\mathbf{x} = (x_1, \dots, x_n)$ be a sequence in $R$, consider
+--   $f_{\mathbf{x}} : R^n \to R, e_i \mapsto x_i$. The Koszul complex $K_{\bullet}(\mathbf{x})$
+--   is defined as $K_{\bullet}(\mathbf{x}) := K_{\bullet}(f_{\mathbf{x}})$. -/
+-- noncomputable def koszulComplex : HomologicalComplex (ModuleCat R) (ComplexShape.down ℕ) :=
+--   _root_.koszulComplex (Fin rs.length →₀ R) <|
+--     Finsupp.linearCombination R (fun (i : Fin rs.length) ↦ rs.get i)
 
-end RingTheory.Sequence
+-- end RingTheory.Sequence
 
-section twisted
+-- section twisted
 
---set_option pp.universes true
-variable {R : Type u} [CommRing R] (M : ModuleCat.{w} R)
+-- --set_option pp.universes true
+-- variable {R : Type u} [CommRing R] (M : ModuleCat.{w} R)
 
-noncomputable def ModuleCat.tensorRight : (ModuleCat.{v} R) ⥤ (ModuleCat.{max v w} R) where
-  obj N := ModuleCat.MonoidalCategory.tensorObj N M
-  map N := ModuleCat.MonoidalCategory.whiskerRight N M
-  map_id _ := ModuleCat.hom_ext (TensorProduct.ext rfl)
-  map_comp _ _ := ModuleCat.hom_ext (TensorProduct.ext rfl)
+-- noncomputable def ModuleCat.tensorRight : (ModuleCat.{v} R) ⥤ (ModuleCat.{max v w} R) where
+--   obj N := ModuleCat.MonoidalCategory.tensorObj N M
+--   map N := ModuleCat.MonoidalCategory.whiskerRight N M
+--   map_id _ := ModuleCat.hom_ext (TensorProduct.ext rfl)
+--   map_comp _ _ := ModuleCat.hom_ext (TensorProduct.ext rfl)
 
-instance : (ModuleCat.tensorRight M).Additive where
-  map_add := by
-    refine ModuleCat.hom_ext (TensorProduct.ext ?_)
-    simp only [tensorRight, ModuleCat.MonoidalCategory.whiskerRight, hom_add, LinearMap.rTensor_add]
-    rfl
+-- instance : (ModuleCat.tensorRight M).Additive where
+--   map_add := by
+--     refine ModuleCat.hom_ext (TensorProduct.ext ?_)
+--     simp only [tensorRight, ModuleCat.MonoidalCategory.whiskerRight, hom_add, LinearMap.rTensor_add]
+--     rfl
 
-variable {R : Type u} [CommRing R] (L : Type v) [AddCommGroup L] [Module R L] (f : L →ₗ[R] R)
-  (M : ModuleCat.{w} R) (n : ℕ)
+-- variable {R : Type u} [CommRing R] (L : Type v) [AddCommGroup L] [Module R L] (f : L →ₗ[R] R)
+--   (M : ModuleCat.{w} R) (n : ℕ)
 
-/-- The Koszul complex with coefficients in $M$ is defined as
-  $K_{\bullet}(f, M) := K_{\bullet}(f)⊗M$. -/
-noncomputable def twistedKoszulComplex :
-    HomologicalComplex (ModuleCat.{max u v w} R) (ComplexShape.down ℕ) :=
-  ((ModuleCat.tensorRight M).mapHomologicalComplex (ComplexShape.down ℕ)).obj (koszulComplex L f)
+-- /-- The Koszul complex with coefficients in $M$ is defined as
+--   $K_{\bullet}(f, M) := K_{\bullet}(f)⊗M$. -/
+-- noncomputable def twistedKoszulComplex :
+--     HomologicalComplex (ModuleCat.{max u v w} R) (ComplexShape.down ℕ) :=
+--   ((ModuleCat.tensorRight M).mapHomologicalComplex (ComplexShape.down ℕ)).obj (koszulComplex L f)
 
-/-- The Koszul homology with coefficients in $M$ is defined as
-  $H_n(f, M) := H_n(K_{\bullet}(f, M))$. -/
-noncomputable def twistedKoszulHomology : ModuleCat.{max u v w} R :=
-  (twistedKoszulComplex L f M).homology n
+-- /-- The Koszul homology with coefficients in $M$ is defined as
+--   $H_n(f, M) := H_n(K_{\bullet}(f, M))$. -/
+-- noncomputable def twistedKoszulHomology : ModuleCat.{max u v w} R :=
+--   (twistedKoszulComplex L f M).homology n
 
-end twisted
+-- end twisted
