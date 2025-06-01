904d64873e1f3f018a9ddc6f10dd508237c3fc1e
Yongle Hu <140475041+mbkybky@users.noreply.github.com>
2025-06-01 12:01:45 +0800

    Update DGA.lean

--- KoszulComplex/DGA.lean
+++ KoszulComplex/DGA.lean
@@ -1,7 +1,30 @@
 import Mathlib
 
-variable {R A : Type*} [CommSemiring R] [CommSemiring A] [Algebra R A] (𝒜 : ℤ → Submodule R A)
+variable {R A : Type*} [CommSemiring R] [CommSemiring A] [Algebra R A] (𝓐 : ℤ → Submodule R A)
 
-class DifferentialGradedAlgebra [GradedAlgebra 𝒜] where
+section DGA
+
+class DifferentialGradedAlgebra [GradedAlgebra 𝓐] where
   deriv : Derivation R A A
-  derivIsHomogeneous (n : ℤ) (m : 𝒜 n) : deriv m ∈ 𝒜 (n - 1)
+  derivIsHomogeneous (n : ℤ) (m : 𝓐 n) : deriv m ∈ 𝓐 (n - 1)
+
+end DGA
+
+section DGM
+
+variable [GradedAlgebra 𝓐] [DifferentialGradedAlgebra 𝓐]
+  {M : Type*} [AddCommMonoid M] [Module R M] [Module A M] [IsScalarTower R A M]
+  (𝓜 : ℤ → Submodule R M)
+
+class GradedModule extends SetLike.GradedSMul 𝓐 𝓜, DirectSum.Decomposition 𝓜
+open DirectSum
+example [GradedModule 𝓐 𝓜] : @LinearEquiv A A _ _ (RingHom.id A) (RingHom.id A) _ _ M (⨁ (i : ℤ), 𝓜 i) _
+    _ _ (by letI := GradedModule.isModule 𝓐 𝓜; infer_instance) := by
+  have := GradedModule.linearEquiv 𝓐 𝓜
+  apply this
+
+-- class DifferentialGradedModule where
+--   deriv :
+
+
+end DGM
